dbeda7165ed855b6b3e9cdec3d8a3fc7265e4692
Samson Jin <apsong@gmail.com>
2014-10-20 14:53:48 +0800

    .bash_ds_functions

--- trunk/.JS/py/doc.query.py
+++ trunk/.JS/py/doc.query.py
@@ -1,19 +1,32 @@
 #! /usr/bin/env python3.4
 
+import threading, logging
+import json, random
 import http.client
-import sys
-import json
+from pymongo import MongoClient
+import argparse
 
-class TestClient:
+parser = argparse.ArgumentParser(description='Start threads to doc.query repeately.')
+parser.add_argument('-t', dest='THREADS', type=int, default=1)
+parser.add_argument('-n', dest='NUM2RUN', type=int, default=1)
+ARGS = parser.parse_args()
+
+logging.basicConfig(format=' [%(asctime)s] %(message)s', datefmt='%H:%M:%S', 
+        level=logging.INFO)
+
+class TestClient(threading.Thread):
     host = None
     port = None
     conn = None
     token = None
+    count = 0
 
     def __init__(self, host, port=None):
+        threading.Thread.__init__(self, name=threading.active_count())
         self.host = host
         self.port = port
         self.conn = http.client.HTTPSConnection(self.host, self.port)
+        #self.conn.set_debuglevel(1)
 
     def close(self):
         self.conn.close()
@@ -23,33 +36,67 @@ class TestClient:
             '{"userid":"%s","password":"%s"}' % (userid, password),\
             {"Content-type":"application/json"})
         resp = self.conn.getresponse()
+        data = resp.read()
         if (resp.code != 200):
-            print("Error: " + str(resp.code) + " " + resp.reason)
+            logging.error("\b@%s  %d: %d %s" % (self.name, self.count,
+                        resp.code, resp.reason))
             return
 
-        data = resp.read().decode()
-        dict = json.loads(data)
+        dict = json.loads(data.decode())
         self.token = dict['response']['token']
+        print(self.name, ":", self.token)
 
     def doc_query(self, merchantid, is_print):
         self.conn.request("POST", "/api?method=doc.query&token="+self.token,\
             '{"query":{"metadata.parameter.merchantid":"%s"}}' % merchantid,\
             {"Content-type":"application/json"})
         resp = self.conn.getresponse()
+        data = resp.read()
         if (resp.code != 200):
-            print("Error: " + str(resp.code) + " " + resp.reason)
+            logging.error("\b@%s  %d: %d %s" % (self.name, self.count,
+                        resp.code, resp.reason))
             return
 
         if (is_print):
-            data = resp.read().decode()
-            dict = json.loads(data)
+            dict = json.loads(data.decode())
             #print(json.dumps(dict, indent=2))
-            print(" ".join([e['filename'] for e in dict['response']]))
+            logging.info("\b@%s  %d: %s => %s" % (self.name, self.count, merchantid, 
+                         " ".join([e['filename'] for e in dict['response']])))
 
-def get_merchantids():
-    pass
+    def run(self):
+        global ARGS
+        count = 0
+        while (count != ARGS.NUM2RUN):
+            self.user_authorize('system', 'manage')
+            count += 1
+#        while True:
+#            global ids
+#            for id in ids:
+#                self.doc_query(id, self.count % 1000 == 0)
+#                self.count+=1
 
-tc = TestClient("192.168.99.241", 9091)
-tc.user_authorize('test', 'test')
-tc.doc_query('999290055110006', True)
+def get_merchantids(host, port, database, username, password):
+    logging.info("Connect to mongo://{3}:{4}@{0}:{1}/{2} for merchantid.".format(
+                host,port,database,username,password))
+    mg = MongoClient(host, port)
+    db = mg[database]
+    db.authenticate(username, password)
+    files = db.fs.files
+    ids = {row["metadata"]["parameter"]["merchantid"]
+           for row in files.find(fields={"metadata.parameter.merchantid":1,"_id":0}, limit=10)}
+    logging.info("Get %d merchantid." % len(ids))
+    ids = list(ids);     logging.info("Listed.")
+    random.shuffle(ids); logging.info("Shuffled.")
+
+    mg.close()
+    return ids
+
+################################## MAIN ###################################
+ids = get_merchantids('192.168.99.241', 40000, 'kydsystem', 'kyd', 'kyd')
+
+for i in range(ARGS.THREADS):
+    tc = TestClient("192.168.99.241", 9091)
+    tc.start()
+tc.join()
 tc.close()
+

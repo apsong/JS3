45ff96f3c88c7a4638e9a983ad920f7ac80d3765
Samson Jin <apsong@gmail.com>
2015-04-17 21:51:35 +0800

    final\!

--- trunk/.JS/py/doc.manage.py
+++ trunk/.JS/py/doc.manage.py
@@ -12,7 +12,7 @@ parser.add_argument('-t', dest='THREADS', type=int, default=1)
 parser.add_argument('-b', dest='BATCH_NUM', type=int, default=6)
 parser.add_argument('-B', dest='BATCH_SECONDS', type=int, default=100)
 parser.add_argument('--report', type=int, default=0)
-parser.add_argument('--no_verify', action='store_true', default=True)
+parser.add_argument('--no_verify', action='store_true', default=False)
 parser.add_argument('-v', action='count', default=0)
 ARGS = parser.parse_args()
 #TODO support batch for both seconds and iterations
@@ -26,7 +26,7 @@ logging.basicConfig(format=' [%(asctime)s.%(msecs)03d@%(processName)s] %(message
 logging.info(ARGS)
 
 class TestClient(multiprocessing.Process):
-    def __init__(self, name, host, port, ids, counter, IS_EXIT):
+    def __init__(self, name, host, port, counter, IS_EXIT):
         multiprocessing.Process.__init__(self, name=name)
         self.host = host
         self.port = port
@@ -34,7 +34,6 @@ class TestClient(multiprocessing.Process):
         self.IS_EXIT = IS_EXIT
         self.conn = HTTPSConnection(self.host, self.port,
                 timeout=ARGS.BATCH_SECONDS*ARGS.BATCH_NUM/2)
-        self.ids = list(ids)
         if ARGS.v>1:
             #self.conn.set_debuglevel(1)
             HTTPConnection.debuglevel=1
@@ -108,7 +107,7 @@ class TestClient(multiprocessing.Process):
                 logging.info("%d: %s => %s => %s" % (self.counter.value, merchantid, id, pdf))
         elif not ARGS.no_verify:
             if expected_pdf not in [e["filename"] for e in response]:
-                logging.warn("VERIFY FAILED: id: %s => expected %s, but got %s" %
+                logging.error("VERIFY FAILED: id: %s => expected %s, but got %s" %
                         (merchantid, expected_pdf, json.dumps(response, indent=2)))
         elif is_print:
             logging.info("%d: %s => %s" % (self.count, merchantid, " ".join([e['filename'] for e in response])))
@@ -143,8 +142,12 @@ class TestClient(multiprocessing.Process):
                     self.counter.value += 1
             self.close()
             return
+
+        global IDS
+        ids=IDS #TODO: gloabl variable affect performance?
+        logging.info("[%s ... %s]" % (ids[0], ids[-1]))
         while True:
-            for id, pdf in self.ids:
+            for id, pdf in ids:
                 if self.IS_EXIT.value:
                     self.close()
                     return
@@ -162,10 +165,11 @@ def get_merchantids_from_db(host, port, database, username, password):
     db.authenticate(username, password)
     db = mg["kyddata"]
     files = db.fs.files
-    ids = {(row["metadata"]["parameter"]["merchantid"],row["filename"])
-        for row in files.find(projection={"metadata.parameter.merchantid":1,"filename":1,"_id":0}, limit=11)}
-    ids = list(ids);     #set -> list
-    logging.info("Get %d merchantid: [%s ... %s]" % (len(ids), ids[0], ids[-1]))
+    ids = {row["metadata"]["parameter"]["merchantid"] : row["filename"]
+        for row in files.find(projection={"metadata.parameter.merchantid":1,"filename":1,"_id":0}, limit=101)}
+    ids = list(ids.items())
+    random.shuffle(ids)
+    logging.info("Get %d (merchantid,pdf): [%s ... %s]" % (len(ids), ids[0], ids[-1]))
 
     mg.close()
     return ids
@@ -177,25 +181,26 @@ def get_merchantids():
         with open(keyfile, 'rb') as f:
             #ids = [line.rstrip() for line in f]
             ids = pickle.load(f)
-        logging.info("Load (id,pdf) tuple from %s" % keyfile)
+        logging.info("Load %d (merchantid,pdf) from %s" % (len(ids), keyfile))
     else:
         ids = get_merchantids_from_db('192.168.99.242', 40000, 'kydsystem', 'kyd', 'kyd')
         with open(keyfile, 'wb') as f:
             pickle.dump(ids, f)
-        logging.info("Save (id,pdf) tuple into %s" % keyfile)
+        logging.info("Save (merchantid,pdf) into %s" % keyfile)
     return ids
 
 ################################## MAIN ###################################
-IDS = get_merchantids()
+if ARGS.ACTION in ['query', 'download']:
+    IDS = get_merchantids()
 IS_EXIT = multiprocessing.Value('i', 0, lock=False)
 
 counters = list(range(ARGS.THREADS))
 tc = list(range(ARGS.THREADS))
 for i in range(ARGS.THREADS):
-    random.shuffle(IDS)
     counters[i] = multiprocessing.Value('i', 0, lock=False)
-    tc[i] = TestClient("%s" % (i+1), "192.168.99.242", 9091, IDS, counters[i], IS_EXIT)
+    tc[i] = TestClient("%s" % (i+1), "192.168.99.242", 9091, counters[i], IS_EXIT)
     tc[i].start()
+    random.shuffle(IDS)
 
 last_counts = [0] * ARGS.THREADS
 last_total  = 0

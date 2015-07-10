ed6dc4250fb750a296962e95944f5653e915acf5
Samson.Jin <apsong@gmail.com>
2015-07-10 11:07:36 +0800

    u

--- trunk/.JS/bin/ccb_upload_projects.sh
+++ trunk/.JS/bin/ccb_upload_projects.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-Azkaban session.reinit || exit 1
+PROGRAM=`basename $0`
+AZKABAN=`dirname $0`/Azkaban
+
+$AZKABAN session.reinit || exit 1
 for PROJECT_DIR in `find -mindepth 1 -maxdepth 1 -type d -name "[a-zA-Z]*"`; do
     PROJECT=`basename $PROJECT_DIR`
-    Azkaban project.create -p $PROJECT
-    Azkaban project.upload -p $PROJECT -z $PROJECT_DIR
+    $AZKABAN project.create -p $PROJECT
+    $AZKABAN project.upload -p $PROJECT -z $PROJECT_DIR
 done
